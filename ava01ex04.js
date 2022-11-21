d8cdd38065e398f3e3ae95a9e1ae666a01c24be3
Gustavo Dias <90855105+gustapine@users.noreply.github.com>
2022-11-21 20:36:06 -0300

    Create ava01ex04.js

---
+++ ava01ex04.js
@@ -0,0 +1,12 @@
+const array = [5, 3, 4, 3, 4];
+
+const encontreSolitario = (arr) => {
+    for (const num of arr) {
+        if (arr.indexOf(num) === arr.lastIndexOf(num)) return num;
+    }
+    return 'No lonely integers.';
+};
+
+
+console.log(array[0],array[1],array[2],array[3],array[4]);
+console.log("Single number: " + encontreSolitario(array));
