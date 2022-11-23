afc8c7a0b4bc282e38bc7db9718bf3abd1cce616
Gustavo Dias <90855105+gustapine@users.noreply.github.com>
2022-11-23 11:03:32 -0300

    commit3

--- ava01ex05.js
+++ ava01ex05.js
@@ -1,3 +1,16 @@
+//Escreva um algoritmo para encontrar um único número de um array onde cada
+//número repete três vezes, exceto um:
+//Input : array(5, 3, 4, 3, 5, 5, 3);
+//Output : array
+//([0] => 5,
+//[1] => 3,
+//[2] => 4,
+//[3] => 3,
+//[4] => 5,
+//[5] => 5,
+//[6] => 3,);
+//Single Number: 4
+
 const array = [5, 3, 4, 3, 5, 5, 3];
 
 const encontreSolitario = (arr) => {
